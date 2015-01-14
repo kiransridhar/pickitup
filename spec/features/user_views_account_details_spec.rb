d39cab771b8f842dde9dea4723303d74ff234538
Kiran Sridhar <kiran7773@hotmail.com>
2015-01-14 14:18:14 -0500

    added styling stuff and modified tests for consistency

--- spec/features/user_views_account_details_spec.rb
+++ spec/features/user_views_account_details_spec.rb
@@ -13,7 +13,7 @@ feature "user sees account details", %{
       visit new_user_session_path
       fill_in "Email", with: user.email
       fill_in "Password", with: user.password
-      click_on "Log in"
+      click_on "Sign in"
 
       visit user_path(user)
 
