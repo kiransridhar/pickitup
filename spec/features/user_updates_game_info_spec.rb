dbca64d3a7f5d6793d4c1c138e0e8b1219136165
Kiran Sridhar <kiran7773@hotmail.com>
2015-01-19 13:16:26 -0500

    Added editing games by users

---
+++ spec/features/user_updates_game_info_spec.rb
@@ -0,0 +1,67 @@
+require "rails_helper"
+
+feature "user updates site", %{
+  As user who created a game,
+  I want to update a game's information
+  So that I can correct errors or provide new information
+  } do
+
+    scenario "authenticated user sees edit button on show page" do
+      edit_game = FactoryGirl.create(:game)
+      visit new_user_session_path
+
+      fill_in "Email", with: edit_game.user.email
+      fill_in "Password", with: "password"
+
+      click_button "Sign in"
+
+      visit game_path(edit_game)
+
+      expect(page).to have_content "Edit"
+
+    end
+    scenario 'unauthenticated user sees edit button on show page' do
+      edit_game = FactoryGirl.create(:game)
+
+      visit game_path(edit_game)
+
+      expect(page).to have_no_content "Edit"
+
+    end
+    scenario 'authenticated user edits game' do
+      edit_game = FactoryGirl.create(:game)
+      visit new_user_session_path
+
+      fill_in "Email", with: edit_game.user.email
+      fill_in "Password", with: "password"
+
+      click_button "Sign in"
+
+
+      visit edit_game_path(edit_game)
+      fill_in "Name", with: "bunt"
+
+
+      click_button "Update Game"
+      expect(page).to have_content "Game updated successfully"
+      expect(page).to have_content "bunt"
+
+    end
+    scenario 'authenticated user unsuccessfully edits game' do
+      edit_game = FactoryGirl.create(:game)
+      visit new_user_session_path
+
+      fill_in "Email", with: edit_game.user.email
+      fill_in "Password", with: "password"
+
+      click_button "Sign in"
+
+      visit edit_game_path(edit_game)
+      fill_in "Name", with: ""
+
+
+
+      click_button "Update Game"
+      expect(page).to have_content "error"
+    end
+  end
